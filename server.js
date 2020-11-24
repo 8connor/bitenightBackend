61c33db19f27e721003549b56ff4ad6892b41d76
8connor <connorh16@gmail.com>
2020-11-24 09:57:50 -0500

    made the starter files for the server.

---
+++ server.js
@@ -0,0 +1,17 @@
+const { json } = require("body-parser");
+const express = require("express");
+const app = express();
+
+const PORT = 4001;
+
+app.use(json());
+
+app.get("/api/test", (req, res) => {
+    console.log(req);
+
+    res.sendStatus(200);
+});
+
+app.listen(PORT, (req, res) => {
+    console.log("LISTENING ON " + PORT);
+});
