e29c451a5132712d7ca5c290300a839ae9fe2045
8connor <connorh16@gmail.com>
2020-11-24 13:34:36 -0500

    not using https for now.

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const { json } = require("body-parser");
 const express = require("express");
 const https = require("https")
 const app = express();
-const fs = require("fs");
 
 const PORT = 4001;
-
-const options = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-}
 
 app.use(express.json());
 
