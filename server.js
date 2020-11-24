feadf574a57fdf8987cf3825f5bae64a2029de21
8connor <connorh16@gmail.com>
2020-11-24 14:08:12 -0500

    now have the backend working with https.

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const https = require("https")
 const app = express();
-
+const https = require("https");
+const fs = require("fs");
 const PORT = 4001;
 
+const options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+}
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
 
 app.get("/api/test", (req, res) => {
     console.log(req.body);
 
-    res.json({
-        hello: "world"
-    })
+    res.json({ hello: "world" });
 });
 
-app.listen(PORT, () => {
+const server = https.createServer(options, app)
+
+server.listen(PORT, () => {
     console.log("LISTENING ON " + PORT);
 });
