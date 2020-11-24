d472e476e0d3119b4e07935baa93f26ada0243bc
8connor <connorh16@gmail.com>
2020-11-24 10:23:16 -0500

    working on getting https set up.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const { json } = require("body-parser");
 const express = require("express");
+const https = require("https")
 const app = express();
+const fs = require("fs")
 
 const PORT = 4001;
+
+const options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+}
 
 app.use(json());
 
@@ -12,6 +19,8 @@ app.get("/api/test", (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(PORT, (req, res) => {
+var server = https.createServer(options, app)
+
+server.listen(PORT, () => {
     console.log("LISTENING ON " + PORT);
 });
